d5cb02b7371ba760dd278d94e3fe51e83e676829
zlezero <tom.new2010@gmail.com>
2018-10-10 21:49:54 +0200

    Ajout de l'utilisation des includes

--- include/head.php
+++ include/head.php
@@ -1,3 +1,9 @@
+<?php session_start(); ?>
+
+<!doctype html>
+
+<html lang="fr">
+
 <head>
     <meta charset="utf-8">
     <title>Evaluation des enseignements</title>
--- index.php
+++ index.php
@@ -1,24 +1,4 @@
-<?php session_start(); ?>
-
-<!doctype html>
-
-<html lang="fr">
-
-<head>
-
-  <meta charset="utf-8">
-
-  <title>Evaluation des enseignements - Connexion</title>
-  <meta name="description" content="Mon Super Site">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-  <link rel="stylesheet" type="text/css" href="style.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-
-</head>
+<?php require_once('include/head.php') ?>
 
 <body>
 
