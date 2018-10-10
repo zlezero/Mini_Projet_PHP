40cfb217c8bc682ff2dafab3f35c3f2d90e2ab96
Poulposaure <33195525+Poulpy@users.noreply.github.com>
2018-10-10 21:25:34 +0200

    ajout d'un pied de page

---
+++ include/footer.php
@@ -0,0 +1,5 @@
+<footer class="footer-copyright py-3 text-center" >
+    <ul>
+        <li style="list-style:none;text-align:center;">© 2018 Copyright<a href="https://github.com/zlezero/Mini_Projet_PHP"><i class="fab fa-github"></i></a></li>
+    </ul>
+</footer>
