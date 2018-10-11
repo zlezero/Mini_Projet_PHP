dce0757190c12a7c2609926fc2a3a010f4f4e2a7
Poulposaure <33195525+Poulpy@users.noreply.github.com>
2018-10-11 16:14:34 +0200

    ajout image et barre de navigation + favicon

--- login.php
+++ login.php
@@ -1,21 +1,14 @@
 <?php
-
 require_once('config.php');
-
 if ( (isset($_POST['id']) && isset($_POST['pwd'])) AND !(empty($_POST['id']) && empty($_POST['pwd'])) ) {
-
     foreach($listeFichiers as $fichier) {
-
         if (file_exists($fichier)) {
-
             $pointeur = fopen($fichier, "r");
-
             while ( ($data = fgetcsv($pointeur)) !== FALSE) {
                 
                 if ( $_POST['id'] == $data[0] && $_POST['pwd'] == $data[1] ) {
                     
                     $_SESSION['id'] = $data[0];
-
                     if ($fichier == $listeFichiers[0]) { #Si il s'agit d'un admin
                         $_SESSION['role'] = "admin";
                         header('Location: admin.php');
@@ -31,24 +24,16 @@ if ( (isset($_POST['id']) && isset($_POST['pwd'])) AND !(empty($_POST['id']) && 
                         header('Location: prof.php');
                         exit;
                     }
-
                 }
                 
             }
-
         }
-
     }
-
     header('Location: index.php?erreur='.sha1("C'est une erreur !"));
     exit;
-
 }
 else {
     header('Location: index.php?erreur='.sha1("C'est une erreur !"));
     exit;
 }
-
-
-
 ?>
--- vote.php
+++ vote.php
@@ -1,20 +1,15 @@
 <?php
-
 require_once("config.php");
 require_once($fichiersInclude.'head.php');
-
 if (!estConnecte() AND !($_SESSION['role'] == "etudiant")) {
     header('Location: index.php');
     exit;
 }
-
 ?>
 
 
 
 
 <?php
-
 require_once($fichiersInclude.'footer.php'); 
-
 ?>
