1ae748e5cdf68104174e104d1ff2849c485385ce
zlezero <tom.new2010@gmail.com>
2018-10-11 09:15:18 +0200

    Fix de sécurité

--- vote.php
+++ vote.php
@@ -3,7 +3,7 @@
 require_once("config.php");
 require_once($fichiersInclude.'head.php');
 
-if (!estConnecte()) {
+if (!estConnecte() AND !($_SESSION['role'] == "etudiant")) {
     header('Location: index.php');
     exit;
 }
