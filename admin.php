7ff87791a3e93140e317b42981d98dfd54d189a6
zlezero <tom.new2010@gmail.com>
2018-10-14 18:34:20 +0200

    Ajout des squelettes des pages prof/admin

---
+++ admin.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once("config.php");
+require_once($fichiersInclude.'head.php');
+
+if (!estConnecte() OR $_SESSION['role'] != "admin") { #Si on arrive sur cette page alors que l'on est pas connecté / ou que l'on n'est pas un administrateur
+    header('Location: index.php'); #On redirige vers la page de connexion
+    exit;
+}
+
+require_once($fichiersInclude.'footer.php'); 
+
+?>
