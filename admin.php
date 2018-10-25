3230f9aec87c755b63377a9711fe1dabccad0da8
Carmenouu <39767851+Carmenouu@users.noreply.github.com>
2018-10-25 16:52:29 +0200

    Page admin
    Mise à jour de la page admin : affichage des votes pour chaque UE

--- admin.php
+++ admin.php
@@ -8,6 +8,81 @@ if (!estConnecte() OR $_SESSION['role'] != "admin") { #Si on arrive sur cette pa
     exit;
 }
 
+$notes = array("1" => "Très mécontent", "2" => "Mécontent", "3" => "Moyen", "4" => "Satisfait", "5" => "Très satisfait"); #Les différentes propositions de vote
+
+//Tableau qui va contenir les différentes UE et les notes correspondantes
+$tabVoteUE = array( "ue1" => array(0, 0, 0, 0, 0),
+					"ue2" => array(0, 0, 0, 0, 0),
+					"ue3" => array(0, 0, 0, 0, 0),
+					"ue4" => array(0, 0, 0, 0, 0),
+					"ue5" => array(0, 0, 0, 0, 0));
+
+//Tableau des totaux
+$tabNbVotes = array ("ue1" => 0,
+					"ue2" => 0,
+					"ue3" => 0,
+					"ue4" => 0,
+					"ue5" => 0,
+					"total" => 0);
+
+
+// Parccours des fichiers de vote
+foreach (glob($fichiersVote."*.csv") as $filename) {
+    $file = file($filename);
+	
+	//A chaque ligne on récupère l'ue et le vote correspondant
+	for ($ligne = 0; $ligne < sizeof($file); $ligne++) {
+		$ligneVote = explode(',', $file[$ligne]);
+		
+		$ue = $ligneVote[0] ;
+		$vote = $ligneVote[1] ;
+		$tabVoteUE[$ue][intval($vote)-1] +=1  ;
+		$tabNbVotes[$ue] +=1 ;
+		$tabNbVotes["total"] +=1 ;
+		
+	}
+
+
+}
+
+//On affiche le tableau de votes pour chaque UE
+foreach ($listeUE as $UE => $matiere) {
+	echo "<div class='jumbotron'>";
+	echo '<h2 class="display-6">'.$UE.'  '.$matiere.'</h2>';
+	echo "<p class='lead'>Total des votes pour cette matière</p>";
+
+	// AFFICHAGE DES VOTES
+
+	echo "<table border='1' cellpadding='20'><tr>";
+	// on affiche les critères de sélection ("très mécontent", etc.)
+	foreach ($notes as $n) {
+		echo '<td><h4 class="display-6">' . $n . '</h4></td>';
+	}
+	echo '<td><h5 class="display-6">TOTAUX</h5></td></tr><tr>';
+	
+	// on affiche les votes
+	foreach ($tabVoteUE[$UE] as $nbVotes) {
+		echo '<td><h6 class="display-6">' . $nbVotes . '</h6></td>';
+	}
+	
+	echo '<td><h6 class="display-6">'.$tabNbVotes[$UE].'</h6></td></tr><tr>';
+	// et on affiche la proportion des votes
+	foreach ($tabVoteUE[$UE] as $nbVotes) {
+		echo '<td><h6 class="display-6">' . 100 * round($nbVotes / $tabNbVotes[$UE], 2) . ' %</h6></td><br><br>';
+	}
+	
+	echo "</table>" ;
+}
+?>
+        </tr>
+    </table>
+    <form class="form-group" action="logout.php" method="post">
+        <button type="submit" class="btn btn-danger" style="margin:20px;">Se déconnecter</button>
+    </form>
+
+</div><!-- jumbotron -->
+
+<?php
 require_once($fichiersInclude.'footer.php'); 
 
 ?>
